5869f2394828374c42ff44bc36240c8ea78d5624
Felipe de Sousa Fernandes <fesousalocutor@gmail.com>
2022-06-05 12:23:45 -0300

    Title e Subtitle instaled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next'
+import Title from '../ui/components/Title/Title'
 
 const Home: NextPage = () => {
   return (
     <div>
-     ta rodadndo
+      <Title title="" subtitle={
+        <span>Com um pequeno valor mensal, você<br />
+          porde <strong>adotar um pet virtualmente</strong>
+        </span>
+      } />
     </div>
   )
 }
---
+++ src/ui/components/Title/Title.tsx
@@ -0,0 +1,18 @@
+import {
+  TitleStyled,
+  SubTitle
+} from './Title.styled'
+
+interface TitleProps {
+  title: String
+  subtitle?: String | JSX.Element
+}
+
+export default function Title(props: TitleProps){
+  return (
+    <>
+      <TitleStyled>{props.title}</TitleStyled>
+      <SubTitle>{props.subtitle}</SubTitle>
+    </>
+  )
+}
