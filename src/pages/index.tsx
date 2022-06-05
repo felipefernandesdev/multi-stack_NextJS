51de77b037507f310af41fd74e3fbb7496f62a02
Felipe de Sousa Fernandes <fesousalocutor@gmail.com>
2022-06-05 11:28:16 -0300

    Logo instaled

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+     ta rodadndo
+    </div>
+  )
+}
+
+export default Home
---
+++ src/ui/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import {
+  HeaderContainer,
+  Logo
+} from './Header.styled'
+
+export default function Header() {
+  return(
+  <HeaderContainer>
+    <Logo src="/images/logo.svg"/>
+    </HeaderContainer>
+  )
+}
